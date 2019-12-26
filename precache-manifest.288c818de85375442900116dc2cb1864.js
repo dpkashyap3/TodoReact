b29c0008bc02d34addc1fb84a7feb74741a38005
dpkashyap3 <32429850+dpkashyap3@users.noreply.github.com>
2019-12-26 22:03:13 +0530

    Updates

---
+++ precache-manifest.288c818de85375442900116dc2cb1864.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af298aeac82628da0c511422511c7374",
+    "url": "/TodoReact/index.html"
+  },
+  {
+    "revision": "a18196f00d21d24e3422",
+    "url": "/TodoReact/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "a18196f00d21d24e3422",
+    "url": "/TodoReact/static/js/2.9d59c866.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/TodoReact/static/js/2.9d59c866.chunk.js.LICENSE"
+  },
+  {
+    "revision": "73b798fbdb1012a194a3",
+    "url": "/TodoReact/static/js/main.30d92762.chunk.js"
+  },
+  {
+    "revision": "4f181fd36a489d61ba2b",
+    "url": "/TodoReact/static/js/runtime-main.ee597ac7.js"
+  }
+]);
